29d68a5e5eb8739ab25dd2f62979f8284bc37535
Gladi023 <Gladimar.Villamil@gmail.com>
2022-06-26 23:21:44 -0400

    lib updated

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,7 +9,7 @@ test('creates an employee object', () => {
     const employee = new Employee('Joe', "", "");
 
     expect(employee.name).toBe('Joe');
-    expect(employee.id).toBe("");
+    expect(employee.employeeId).toBe("");
     expect(employee.email).toBe("");
 });
 
